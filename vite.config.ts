3e079c64a3c086c14892b16ec332b792e1230f05
Vu Nam Hung <vunamhung@gmail.com>
2025-03-26 00:58:11 +0700

    feat(vite.config.ts): add svgr plugin for handling SVG files
    Add svgr plugin to handle SVG files in the Vite configuration. This plugin
    provides options for optimizing SVG files and converting them to React
    components.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,27 @@ import { cloudflare } from '@cloudflare/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [cloudflare({ viteEnvironment: { name: 'ssr' } }), tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        icon: false,
+        memo: true,
+        expandProps: 'end',
+        svgoConfig: {
+          floatPrecision: 2,
+          plugins: [{ name: 'preset-default', params: { overrides: { removeViewBox: false } } }],
+        },
+      },
+      include: '**/*.svg?react',
+    }),
+  ],
 });
