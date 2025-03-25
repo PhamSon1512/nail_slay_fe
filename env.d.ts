3b0f4ddecb93bf969f7152bc978eb944840db8f5
Vu Nam Hung <vunamhung@gmail.com>
2025-03-26 00:57:51 +0700

    chore: update dependencies in package.json
    Add @svgr/plugin-jsx@8.1.0 and @svgr/plugin-svgo@8.1.0
    Update vite-plugin-svgr to version 4.3.0
    Downgrade wrangler to version 3.114.2

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_HOST: string;
+  readonly VITE_RESEND_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
