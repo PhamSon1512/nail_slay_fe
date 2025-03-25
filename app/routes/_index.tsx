e3352bb0caae6df87c5cfd2dd2d6d368cd8470ff
Vu Nam Hung <vunamhung@gmail.com>
2025-03-26 00:11:43 +0700

    refactor: rename home.tsx to _index.tsx for consistency and clarity

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,10 @@
-import type { Route } from './+types/home';
+import type { Route } from './+types/_index';
 import { Welcome } from '~/welcome/welcome';
 
-export function meta({}: Route.MetaArgs) {
-  return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
-}
+export const meta = ({}: Route.MetaArgs) => [
+  { title: 'New React Router App' },
+  { name: 'description', content: 'Welcome to React Router!' },
+];
 
 export function loader({ context }: Route.LoaderArgs) {
   return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
