d4a0064fe8a32ce731e76634fa727e34eaa99f87
Vu Nam Hung <vunamhung@gmail.com>
2025-03-25 23:58:07 +0700

    chore(app): update link text color to dark green in welcome.tsx

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -18,7 +18,7 @@ export function Welcome({ message }: { message: string }) {
               {resources.map(({ href, text, icon }) => (
                 <li key={href}>
                   <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
+                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-green-600"
                     href={href}
                     target="_blank"
                     rel="noreferrer"
