5d4ea84f1a81e6ca195321954e48dbde4fae712a
Vu Nam Hung <vunamhung@gmail.com>
2025-03-26 08:59:15 +0700

    feat(load-context): add Cloudflare type to AppLoadContext
    feat(vite.config): update cloudflare plugin to use cloudflareDevProxy
    
    chore(package.json): update react-router and wrangler versions
    
    feat(functions): add onRequest handler for createPagesFunctionHandler

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { cloudflare } from '@cloudflare/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
@@ -7,7 +7,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    cloudflareDevProxy({
+      getLoadContext({ context }) {
+        return { cloudflare: context.cloudflare };
+      },
+    }),
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
