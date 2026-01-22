84b4ec7e9c266316cd653bcb342d0d3cd694a063
Vu Nam Hung <vunamhung@gmail.com>
2026-01-22 15:20:58 +0700

    chore: update package dependencies versions
    feat: add new future flags in react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,8 @@ import type { Config } from '@react-router/dev/config';
 export default {
   ssr: true,
   future: {
-    unstable_viteEnvironmentApi: true,
+    unstable_optimizeDeps: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
   },
 } satisfies Config;
